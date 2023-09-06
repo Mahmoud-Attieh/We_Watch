0b0e64cb781dc37ba49ca86cc47c4a506fdc66a6
p1gt <ranaranap59@gmail.com>
2023-09-06 17:11:12 +0300

    style

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import io from 'socket.io-client'
 import PageHeader from '../page-header/PageHeader';
+import './chat.scss'
 
 const Chat = (props) => {
     const [socket] = useState(() => io(':8000'));
@@ -32,18 +33,18 @@ const Chat = (props) => {
       <PageHeader>
       My Favorites
     </PageHeader>
-        <div className='login-page' style={{ width: "82%", maxHeight: "300px", overflow: "auto", margin: "0 auto", border: "1px solid black" }}>
+        <div className='chat'>
             <h1>Welcome, {props.name}</h1>
             {
                 messages.map((msg, i) =>
                     (msg.name === props.name) ?
-                        <div key={i} style={{ width: "40%", margin: "1% 0 1% auto", border: "1px solid black", backgroundColor: "#9fc5f8", boxShadow: "3px 3px 3px #777", borderRadius: "10px" }}>
-                            <span><strong>You: </strong></span><br /> {msg.msg}
-                        </div>
+                        <>
+                            <span><strong>You: </strong></span><br /> {msg.msg}<br/>
+                        </>
                         :
-                        <div key={i} style={{ width: "40%", margin: "1% auto 1% 0", border: "1px solid black", backgroundColor: "#dddddd", boxShadow: "3px 3px 3px #777", borderRadius: "10px" }}>
-                            <span><strong>{msg.name}</strong></span> {msg.name ? <>said: </> : <></>} <br /> {msg.msg}
-                        </div>
+                        <>
+                            <span><strong>{msg.name}</strong></span> {msg.name ? <>said: </> : <></>} <br /> {msg.msg} <br/>
+                        </>
                 )
             }
             <form onSubmit={handelSubmit}>
